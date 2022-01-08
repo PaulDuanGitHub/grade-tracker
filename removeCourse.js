dc9a99182aa49d767fbbca645a4b76a7ad13ed6d
Xianzhao Duan <duanxianzhao@gmail.com>
2022-01-08 18:02:55 -0500

    remove files

--- removeCourse.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('removeCourse').addEventListener('click',function(){
-    var courseNums = document.getElementsByClassName('course').length
-    if (courseNums != 0){
-        document.getElementById('main').removeChild(document.getElementById('main').lastElementChild)
-    }
-})
